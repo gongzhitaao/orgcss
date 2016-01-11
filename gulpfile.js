c10de73131e65e6b7c05b14584ee9c9e748c11e9
Zhitao Gong <zhitaao.gong@gmail.com>
2016-01-11 14:03:17 -0600

    td padding

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-var minifyCSS = require('gulp-minify-css');
+var cssnano = require('gulp-cssnano');
 var autoprefixer = require('gulp-autoprefixer');
 var filter = require('gulp-filter');
 var flatten = require('gulp-flatten');
 var browsersync = require('browser-sync');
+var path = require('path');
 
 var src = 'app/css';
 var dist = 'dist';
 
 gulp.task('default', ['browser-sync'], function() {
-    gulp.watch(src + '/*.css', ['optimize-css']);
+  gulp.watch(path.join(src, '*.css'), ['optimize-css']);
 });
 
 gulp.task('browser-sync', function() {
@@ -20,8 +21,8 @@ gulp.task('browser-sync', function() {
     },
     port: 4000,
     files: [
-      'dist/*.css',
-      'dist/*.html'
+      path.join(dist, '*.css'),
+      path.join(dist, '*.html')
     ]
   });
 });
@@ -30,17 +31,22 @@ gulp.task('optimize-css', function() {
   var f0 = filter(['*', '!org-default.css'], {restore: true});
   var f1 = filter(['*', '!org.css'], {restore: true});
 
-  return gulp.src(src + '/*.css')
+  return gulp.src(path.join(src, '*.css'))
     .pipe(f0)
-    .pipe(minifyCSS())
+    .pipe(cssnano())
     .pipe(autoprefixer({ browsers: ['last 2 version']}))
     .pipe(concat('org.css'))
     .pipe(f0.restore)
     .pipe(f1)
-    .pipe(minifyCSS())
+    .pipe(cssnano())
     .pipe(autoprefixer({ browsers: ['last 2 version']}))
     .pipe(concat('org-default.css'))
     .pipe(f1.restore)
     .pipe(flatten())
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('copy', function() {
+  return gulp.src(path.join(dist, '*'))
+    .pipe(gulp.dest('../gh-pages'));
 });
