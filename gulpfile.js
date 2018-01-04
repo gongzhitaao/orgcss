9806e17c2c87b85ce35507b3103443a67558e484
Zhitao Gong <zhitaao.gong@gmail.com>
2018-01-04 17:03:10 -0600

    fix bibliography back reference issue

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,18 @@
 const $ = require('gulp');
 const $changed = require('gulp-changed');
+const $filter = require('gulp-filter');
 const $flatten = require('gulp-flatten');
 const $htmlmin = require('gulp-htmlmin');
+const $plumber = require('gulp-plumber');
 const $postcss = require('gulp-postcss');
+const $replace = require('gulp-replace');
 
 const del = require('del');
 const server = require('browser-sync').create();
 
 $.task('build', $.series(clean, $.parallel(pages, styles, misc)));
 $.task('default', $.series('build', $.parallel(serve, watch)));
-$.task('publish', $.series(publish));
+$.task('publish', publish);
 
 function clean() {
   return del(['build']);
@@ -21,10 +24,9 @@ function reload(done) {
 }
 
 function watch() {
-  $.watch('./src/index.html', $.series(pages, reload));
-  $.watch('./src/css/*.css',
-          $.series(styles, reload));
-  $.watch(['./src/img/*'], $.series(misc, reload));
+  $.watch(['src/index.html', 'src/ref_bib.html'], $.series(pages, reload));
+  $.watch('src/css/*.css', $.series(styles, reload));
+  $.watch(['src/img/*'], $.series(misc, reload));
 }
 
 function serve(done) {
@@ -33,21 +35,26 @@ function serve(done) {
 }
 
 function pages() {
-  return $.src('src/index.html')
+  const f = $filter('src/ref_bib.html', {restore: true});
+  return $.src(['src/index.html', 'src/ref_bib.html'])
     .pipe($changed('build'))
+    .pipe($plumber())
     .pipe($htmlmin({
       removeComments: true,
       collapseWhitespace: true,
       removeEmptyAttributes: true,
       minifyJS: true,
-      minifyCSS: true
-    }))
+      minifyCSS: true}))
+    .pipe(f)
+    .pipe($replace('ref.html', 'index.html'))
+    .pipe(f.restore)
     .pipe($.dest('build'));
 }
 
 function styles() {
   return $.src(['src/css/org-default.css', 'src/css/org.css'])
     .pipe($changed('build'))
+    .pipe($plumber())
     .pipe($postcss([
       require('precss'),
       require('cssnano')({
